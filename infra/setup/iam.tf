33922470aafaaabba819e9029e3a74f52bc354e5
sabioniac <sabionweb@gmail.com>
2025-10-07 20:48:47 +0000

    Update iam.tf

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -3,54 +3,89 @@
 ######################################################################
 
 resource "aws_iam_user" "CD" {
-   name = "devops-cicd"
+  name = "devops-cicd"
 }
 
 resource "aws_iam_access_key" "CD" {
-    user = aws_iam_user.cd.name
+  user = aws_iam_user.CD.name
 }
 
 ###########################################################
 # Policy for Terraform backend to S3 and Dynamo DB Access #
 ###########################################################
 
-data "aws_iam_policy_document" "tf_backend"{
-    statement {
-        effect = "Allow"
-        actions = ["s3:ListBucket"]
-        resources =  ["arn:aws:s3:::${var.tf_state_bucket}"]
-    }
+data "aws_iam_policy_document" "tf_backend" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:ListBucket", "s3:GetBucketLocation"]
+    resources = ["arn:aws:s3:::${var.tf_state_bucket}"]
+  }
 
-    statement {
-        effect = "Allow"
-        actions = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
-        resources = [
-            "arn:aws:s3:::${var.tf_state_bucket}/devops-tf-state-deploy/*",
-            "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy-env/*"
-        ]
-    }
+  statement {
+    effect  = "Allow"
+    actions = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
+    resources = [
+      "arn:aws:s3:::${var.tf_state_bucket}/devops-tf-state-deploy/*",
+      "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy-env/*"
+    ]
+  }
 
-    statement {
-        effect = "Allow"
-        actions= [
-            "dynamodb:DescribeTable",
-            "dynamodb:GetItem",
-            "dynamodb:PutItem",
-            "dynamodb:DeleteItem"
-        ]
-        resources = "arn:aws:dyanmodb:*:*:table/${var.tf_state_lock_table}"
-    }
+  statement {
+    effect = "Allow"
+    actions = [
+      "dynamodb:DescribeTable",
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:DeleteItem"
+    ]
+    resources = ["arn:aws:dyanmodb:*:*:table/${var.tf_state_lock_table}"]
+  }
 }
 
 resource "aws_iam_policy" "tf_backend" {
-    name = "${aws_iam_user.cd.name}-tf-s3-dyanmodb"
-    description = "allow user to use S3 and DynamoDB for TF backend resources"
-    policy = data.aws_iam_policy_document.tf_backend.json
+  name        = "${aws_iam_user.CD.name}-tf-s3-dyanmodb"
+  description = "allow user to use S3 and DynamoDB for TF backend resources"
+  policy      = data.aws_iam_policy_document.tf_backend.json
 }
 
 resource "aws_iam_user_policy_attachment" "tf_backend" {
-    user = aws_iam_user.cd.name
-    policy_arn = aws_iam_policy.tf_backend.arn
+  user       = aws_iam_user.CD.name
+  policy_arn = aws_iam_policy.tf_backend.arn
+}
+#########################
+# Policy for ECR access #
+#########################
+
+data "aws_iam_policy_document" "ecr" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ecr:GetAuthorizationToken"]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:CompleteLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:InitiateLayerUpload",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:PutImage"
+    ]
+    resources = [
+      aws_ecr_repository.app.arn,
+      aws_ecr_repository.proxy.arn,
+    ]
+  }
 }
 
+resource "aws_iam_policy" "ecr" {
+  name        = "${aws_iam_user.CD.name}-ecr"
+  description = "Allow user to manage ECR resources"
+  policy      = data.aws_iam_policy_document.ecr.json
+}
 
+resource "aws_iam_user_policy_attachment" "ecr" {
+  user       = aws_iam_user.CD.name
+  policy_arn = aws_iam_policy.ecr.arn
+}
