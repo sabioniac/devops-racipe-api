7de5e9081a08d5889b88475bbe919ef84797a9af
sabioniac <sabionweb@gmail.com>
2025-10-07 20:49:22 +0000

    Update main.tf

--- infra/setup/main.tf
+++ infra/setup/main.tf
@@ -1,30 +1,27 @@
+#####################
 terraform {
-    required_providers{
-        aws = {
-            source = "hashicorp/aws"
-            version = "5.23.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.23.0"
     }
-      
-      backend "s3" {
-        bucket = "devops-terrafrom-state"
-        key = "devops-tf-state-setup"
-        region = "us-east-1"
-        encrypt = true
-        dynamodb_table = "devops-lockTable"
-        
-      }
- }
+  }
 
- provider "aws" {
-   region = "us-east-1"
+  backend "s3" {
+    bucket         = "devops-terrafrom-state"
+    key            = "devops-tf-state-setup"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "devops-lockTable"
 
-   default_tags {
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
     tags = {
-      Environment = Terraform_workspace
-      auto-delete = no
       ManageBy = "Terraform/Setup"
     }
-     
   }
 }
